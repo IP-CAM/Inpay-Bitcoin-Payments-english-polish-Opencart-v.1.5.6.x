91c74f027e147a255b109e567099bed730e5d9fc
Lech Wilczynski <lech.wilczynski@gmail.com>
2014-07-06 22:00:55 +0200

    polish translation

---
+++ admin/language/polish/payment/inpay.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']      = 'Bitcoin (InPay)';
+
+// Text 
+$_['text_payment']       = 'Płatność';
+$_['text_success']       = 'Sukces: Zmodyfikowano dane modułu płatności Bitcoin (InPay)';
+$_['text_yes']          = 'Tak';
+$_['text_no']          = 'Nie';
+
+$_['text_tranzila']		= '<img src="view/image/payment/inpay.png" alt="Bitcoin (InPay)" title="Bitcoin (InPay)" style="border: 1px solid #EEEEEE;" />';
+
+// Entry
+$_['entry_api_key']       = 'Klucz API:';
+$_['entry_secret_key']   = 'Sekretny klucz API:';
+$_['entry_gateway_url']    = 'Adres url API:';
+
+$_['entry_order_status'] = 'Status zamówienia:';
+$_['entry_geo_zone']     = 'Strefa:';
+$_['entry_status']       = 'Status:';
+$_['entry_sort_order']   = 'Sortowanie:';
+
+// Error
+$_['error_permission']   = 'Brak uprawnień do dokonania zmian';
+$_['error_api_key']        = 'Klucz API wymagany';
+$_['error_secret_key']        = 'Sekretny klucz API wymagany!';
+$_['error_gateway_url']        = 'Wymagane podanie adresu URL API!';
+?>
